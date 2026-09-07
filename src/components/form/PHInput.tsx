f24ecd13fcbe553bd5547701b3b7e5abacb135fe
Nadim-Nion <nadim15-1746@diu.edu.bd>
2026-09-07 21:47:20 +0600

    feat: PHInput wrapper component added

---
+++ src/components/form/PHInput.tsx
@@ -0,0 +1,33 @@
+import { Form, Input } from "antd";
+import { Controller } from "react-hook-form";
+
+type TInputProps = {
+  type: string;
+  name: string;
+  label?: string;
+  disabled?: boolean;
+};
+
+const PHInput = ({ type, name, label, disabled }: TInputProps) => {
+  return (
+    <div style={{ marginBottom: "20px" }}>
+      <Controller
+        name={name}
+        render={({ field, fieldState: { error } }) => (
+          <Form.Item label={label}>
+            <Input
+              disabled={disabled}
+              type={type}
+              id={name}
+              {...field}
+              size="large"
+            />
+            {error && <small style={{ color: "red" }}>{error.message}</small>}
+          </Form.Item>
+        )}
+      />
+    </div>
+  );
+};
+
+export default PHInput;
